c4981fe0f00c3a6fb1f4568f48d99db74348690c
Nabilaboobacker <132455030+Nabilaboobacker@users.noreply.github.com>
2024-10-20 01:37:56 +0400

    db 3

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.nabilaboobacker.in', '.vercel.app']
+ALLOWED_HOSTS = ['nabilaboobacker.in', '.vercel.app']
 
 
 # Application definition
